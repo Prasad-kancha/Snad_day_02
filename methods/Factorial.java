99729ab188bca6e44371eb123cfb83d061edb6e8
Prasad-kancha <prasad.kancha@snaddevelopers.com>
2026-05-23 09:52:41 +0530

    Update Factorial.java

--- methods/Factorial.java
+++ methods/Factorial.java
@@ -1,5 +1,20 @@
 package methods;
+import java.util.*;
 
 public class Factorial {
-
+ static int fact(int n){
+	 if(n==0) {
+		 return 1;
+	 }
+	 
+	 return n * fact(n-1);
+ }
+ public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.print("Give number :");
+	int n=sc.nextInt();
+	System.out.println("factorial of number is "+ fact(n));
+	sc.close();
+}
 }
